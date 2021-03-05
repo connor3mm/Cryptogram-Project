8b10ea993cced7e206289174e8fb3da42c14eaeb
qrb19184 <stewart.lindsay.2019@uni.strath.ac.uk>
2021-03-05 14:01:49 +0000

    Added basic attributes and methods to Players class.

--- src/cryptogram/Players.java
+++ src/cryptogram/Players.java
@@ -1,4 +1,36 @@
 package cryptogram;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Players {
+
+    private List<String> allPlayers = new ArrayList<String>();
+    private File playersFile;
+
+public void addPlayer(File playersFile){
+
+}
+
+public void savePlayers(File playersFile){
+
+}
+
+public void findPlayer(File playersFile){
+
+}
+
+public void getAllPlayersAccuracies(List allPlayers){
+
+}
+
+public void getAllPlayersCryptogramsPlayed(List allPlayers){
+
+}
+
+public void getAllPlayersCompletedCryptos(List allPlayers){
+
+}
+
 }
